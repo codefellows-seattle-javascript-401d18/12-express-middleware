da83b0390aec777375aaa7a1ba730b89d28ef0c4
saidmattar <ssabdou@gmail.com>
2017-09-05 15:20:15 -0700

    created a post and fetone

---
+++ lab-said/lib/storage.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const debug = require('debug')('http:storage');
+const createError = require('http-errors');
+const Toy = require('../model/toy');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+
+const storage = module.exports = {};
+
+storage.create = function(item) {
+  debug('#create');
+
+
+  return new Promise((resolve, reject) => {
+
+    if(!item.name) return reject(createError(400, 'cannot create; name required'));
+    if(!item.desc) return reject(createError(400, 'cannot create; desc required'));
+
+    let toy = new Toy(item.name, item.desc, item.color, item.manif);
+
+    return fs.writeFileProm(`${__dirname}/../data/toy/${toy._id}.json`, JSON.stringify(toy))
+      .then(() => resolve(toy))
+      .catch(reject);
+  });
+};
+
+storage.fetchOne = function(itemId) {
+  debug('#fetchOne');
+
+  return new Promise((resolve, reject) => {
+    // if(!schema) return reject(new Error('cannot get item; schema required'))
+    if(!itemId) return reject(createError(400, 'cannot get item; itemId required'));
+
+    return fs.readFileProm(`${__dirname}/../data/toy/${itemId}.json`)
+      .then(buff => {
+        try {
+          let toy = JSON.parse(buff.toString());
+          return resolve(toy);
+        } catch(e) {
+          return reject(e);
+        }
+      })
+      .catch(reject);
+  });
+};
