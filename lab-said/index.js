fa5b6c7a06c8bfdd5764504c6d5da4437a6b530e
saidmattar <ssabdou@gmail.com>
2017-09-05 20:47:57 -0700

    modiefied index.js

--- lab-said/index.js
+++ lab-said/index.js
@@ -4,4 +4,4 @@ const debug = require('debug')('http:index');
 const server = require('./lib/server');
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => debug(`Listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
